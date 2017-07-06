7d92796f16a40058d5b663c50e850bd21b517fbf
Yuhei mac <yumasingingAgmail.com>
2017-07-06 20:10:36 +0900

    [delete] コンソールクラスの不要な機能を削除

--- src/console/ConsoleSocket.java
+++ src/console/ConsoleSocket.java
@@ -11,7 +11,6 @@ import java.util.concurrent.Executor;
 
 public class ConsoleSocket implements Runnable{
 	private ServerSocket listen = null;
-	private static String PASSWORD = "chiba.robot.studio";
 	private BufferedReader in = null;
 	private PrintWriter out = null;
 
@@ -38,7 +37,7 @@ public class ConsoleSocket implements Runnable{
 		
 		while(true){
 			try {
-				boolean login = false;
+				boolean login = true;
 				
 				// クライアントからの接続要求を待つ。
 				System.out.println("CONSOLE : wait for console connection...");
@@ -49,40 +48,6 @@ public class ConsoleSocket implements Runnable{
 				// 入出力ストリームの生成
 				in  = new BufferedReader(new InputStreamReader(soc.getInputStream()) );
 				out = new PrintWriter(soc.getOutputStream(), true);
-				
-				// -----------------------------------------------------------
-				// 認証 -------------------------------------------------------
-				// 法を256としたアフィン暗号
-				// password : chiba.robot.studio
-				
-				// 鍵の生成
-				int[] i_key = new int[2];
-				i_key[0] = ( (int)(Math.random()*128) ) * 2 + 1 ;
-				i_key[1] = (int)( Math.random() * 256 );
-				System.out.println("key = " + i_key[0] + "," + i_key[1]);
-				// 鍵を送信
-				out.println( "" + i_key[0] + "," + i_key[1]);
-				
-				// パスワード暗号文の生成と確認
-				String recv_pass = in.readLine();
-				char[] true_pass = new char[PASSWORD.length()];
-				for(int i = 0; i < PASSWORD.length(); i++){
-					true_pass[i] = (char)( (PASSWORD.charAt(i)*i_key[0] + i_key[1])%256);
-				}
-				if( recv_pass != null ){
-					//if( Arrays.equals(recv_pass.toCharArray(), true_pass) ){
-					if( recv_pass.equals(PASSWORD) ){
-						System.out.println("login successed.");
-						out.println("Login : ACK");
-						login = true;
-					}else{
-						System.out.println("login failed.");
-						out.println("Login : NAK");
-						login = false;
-					}
-				}else{
-					login = false;
-				}
 				
 				// コンソールからの命令を処理
 				while(login){
